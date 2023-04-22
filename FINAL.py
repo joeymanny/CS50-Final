162efab611d9f06d67d5c048c2a1d3531c5f6361
joeymanny <116774114+joeymanny@users.noreply.github.com>
2023-04-22 12:56:38 -0800

    typo

--- FINAL.py
+++ FINAL.py
@@ -28,7 +28,7 @@ while True:
     height = width
     xslide = float(input("x slide: "))
     yslide = float(input("y slide: "))
-    # initializ array in the shape desired
+    # initialize array in the shape desired
     coords = np.zeros((height, width))
     # for each item in the array (every pixel)
     for y in range(len(coords)//-2, len(coords)):
